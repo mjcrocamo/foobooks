14978dfdefb993dc92ff2394c7523417e0e90a6f
Maggie Crocamo <mjcrocamo@gmail.com>
2018-10-19 21:01:07 -0400

    progress

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        return view('books.index');
+    }
+
+    public function show($title)
+    {
+        return view('books.show')->with(['title' => $title]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController');
+
+Route::get('/books/{title}', 'BookController@show');
+Route::get('/books', 'BookController@index');
